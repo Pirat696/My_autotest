90454aa35aeb7afa1a1eb951789dbd37ff265443
Alexander Ivchenko <Grafsimantika69@gmail.com>
2023-02-10 12:44:23 +0300

    update test

--- tests/test_del_user.py
+++ tests/test_del_user.py
@@ -1,3 +1,7 @@
+from used_data.api.api_urls import POST_CREAT_USER_URL
+import requests
+
+
 def test_del_user(post_creat_user):
     endpoint = response.json()["list"][0]["id"]
     delete_url = f"{POST_CREAT_USER_URL}/{endpoint}"
