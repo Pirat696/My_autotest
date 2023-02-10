f1a79d343467c22844481b519042b4df004cb36d
Alexander Ivchenko <Grafsimantika69@gmail.com>
2023-02-10 13:44:31 +0300

    fixed_bugs

--- tests/test_del_user.py
+++ tests/test_del_user.py
@@ -2,8 +2,8 @@ from used_data.api.api_urls import POST_CREAT_USER_URL
 import requests
 
 
-def test_del_user(post_creat_user):
-    endpoint = response.json()["list"][0]["id"]
-    delete_url = f"{POST_CREAT_USER_URL}/{endpoint}"
+def test_del_user(post_creat_user,get_bearer_token):
+
+    delete_url = f"{POST_CREAT_USER_URL}/{post_creat_user}"
     response = requests.delete(url=delete_url, headers=get_bearer_token)
     assert response.status_code == 204
