13597e9d88379b6664da7ed28c01bee9e19ae830
Alexander Ivchenko <Grafsimantika69@gmail.com>
2023-02-12 01:00:35 +0300

    add_new_del_validations

--- tests/test_del_user.py
+++ tests/test_del_user.py
@@ -1,9 +1,23 @@
-from used_data.api.api_urls import POST_CREAT_USER_URL
 import requests
 
+from used_data.api.api_urls import GET_USER_INFO_URL
+from used_data.api.api_urls import POST_CREAT_USER_URL
 
-def test_del_user(post_creat_user,get_bearer_token):
+def test_chek_user_active(post_creat_user, get_bearer_token):
+    chek_user_active_url = f"{GET_USER_INFO_URL}{post_creat_user}"
+    response = requests.get(url=chek_user_active_url, headers=get_bearer_token)
+    assert response.status_code == 200
+    assert response.json()["list"][0]["is_active"] == True
 
-    delete_url = f"{POST_CREAT_USER_URL}/{post_creat_user}"
+def test_del_user(post_creat_user, get_bearer_token):
+
+    delete_url = f"{POST_CREAT_USER_URL}{post_creat_user}"
     response = requests.delete(url=delete_url, headers=get_bearer_token)
     assert response.status_code == 204
+
+def test_chek_user_inactive(post_creat_user, get_bearer_token):
+
+    chek_user_active_url = f"{GET_USER_INFO_URL}{post_creat_user}"
+    response = requests.get(url=chek_user_active_url, headers=get_bearer_token)
+    assert response.status_code == 200
+    assert response.json()["list"][0]["is_active"] == False
